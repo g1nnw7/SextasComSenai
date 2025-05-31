71beaadb48c56de6387ff6ba6af65b251cdf7c80
g1nnw7 <numseiquenomecoloca@gmail.com>
2025-05-30 21:54:35 -0300

    aula02

---
+++ aula02/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const[valor, setValor] = useState(99)
+  const[inputMarujos, setInputMarujos] = useState('')
+  const[inputEventos, setInputEventos] = useState('')
+  const[viagem, setViagem] = useState()
+  function verificar(){
+    if(inputMarujos >=10 || inputEventos >=1){
+      setViagem('Sim')
+    }
+    else{
+      setViagem('Não')
+    }
+  }
+  return (
+    <>
+    <div>
+      {/* {valor}
+      <button onClick={() => setValor(valor+1)}>+</button> */}
+      <h2>Ex. 9 Capitão Ganso</h2>
+      Marujos: 
+      <input type="text"
+      value={inputMarujos}
+      onChange={(e) => setInputMarujos(e.target.value)}
+      />
+
+      Eventos:
+      <input type="text" 
+      value={inputEventos}
+      onChange={(e) => setInputEventos(e.target.value)}
+      />
+
+      <button onClick={verificar}>Verificar</button>
+        <p>Viagem: {viagem}</p>
+
+      </div>
+    </>
+  )
+}
+
+export default App
